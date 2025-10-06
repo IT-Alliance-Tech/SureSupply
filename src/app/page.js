df9ddce6b2f8dbab4b54c6baae9dc32f1cf9a0f7
Lawtistic <167568623+Lawtistic@users.noreply.github.com>
2025-10-06 15:45:44 +0530

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import HeroSection from "@/components/home/heroSection";
-import HomeAbout from "@/components/home/homeAbout";
-import WhySureSupply from "@/components/home/WhySureSupply";
-import Navbar from "@/components/layout/navbar";
-import Footer from "@/components/layout/footer";
-import QuoteForm from "@/components/home/quote";
-import ManufacturingProcesses from "@/components/home/ManufacturingProcesses";
+import HeroSection from "../components/home/heroSection";
+import HomeAbout from "../components/home/homeAbout";
+import WhySureSupply from "../components/home/WhySureSupply";
+import Navbar from "../components/layout/navbar";
+import Footer from "../components/layout/footer";
+import QuoteForm from "../components/home/quote";
+import ManufacturingProcesses from "../components/home/ManufacturingProcesses";
 
 export default function Home() {
   return (
