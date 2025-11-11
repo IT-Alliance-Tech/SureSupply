c39b2678240f9ec5dde955b43cd40adaa7ff465f
Lavanya <lavilavanya384@gmail.com>
2025-11-11 22:53:27 +0530

    Updated industires we serve page

---
+++ src/app/industriesWeServe/page.js
@@ -0,0 +1,14 @@
+'use client';
+
+import HeroSection from '../../components/IndustriesWeServe/heroSection';
+import IndustriesWeServeSection from '../../components/IndustriesWeServe/industriesWeServeSection';
+
+
+export default function IndustriesPage() {
+  return (
+    <main className="flex flex-col">
+      <HeroSection />
+      <IndustriesWeServeSection />
+    </main>
+  );
+}
